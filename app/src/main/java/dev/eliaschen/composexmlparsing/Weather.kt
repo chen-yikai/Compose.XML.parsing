54ab0fbb6f13766c1256b675d4c8a975b099ff99
elias <eliaschen.dev@gmail.com>
2025-09-11 00:59:28 +0800

    add footer

--- app/src/main/java/dev/eliaschen/composexmlparsing/Weather.kt
+++ app/src/main/java/dev/eliaschen/composexmlparsing/Weather.kt
@@ -1,6 +1,8 @@
 package dev.eliaschen.composexmlparsing
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,19 +10,28 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -29,6 +40,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -46,14 +58,33 @@ fun WeatherScreen() {
     val pagerState = rememberPagerState { data.cityList.size }
 
     Box(modifier = Modifier.fillMaxSize()) {
-        FilledTonalButton(
-            onClick = { nav.navTo(Screen.Home) },
+        Box(
             modifier = Modifier
                 .zIndex(10f)
-                .navigationBarsPadding()
+                .fillMaxWidth()
                 .align(Alignment.BottomCenter)
+                .navigationBarsPadding()
+                .padding(10.dp),
         ) {
-            Text("Back to home")
+            IconButton(onClick = {
+                nav.navTo(Screen.Home)
+            }, modifier = Modifier.align(Alignment.CenterStart)) {
+                Icon(
+                    Icons.Outlined.Home,
+                    contentDescription = null
+                )
+            }
+            Row(modifier = Modifier.align(Alignment.Center)) {
+                repeat(data.cityList.size) {
+                    Box(
+                        modifier = Modifier
+                            .padding(horizontal = 5.dp)
+                            .size(10.dp)
+                            .clip(CircleShape)
+                            .background(if (pagerState.currentPage == it) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.tertiaryContainer)
+                    )
+                }
+            }
         }
         HorizontalPager(pagerState) { page ->
             val weather = data.weathers[data.cityList[page].fileName]!!
